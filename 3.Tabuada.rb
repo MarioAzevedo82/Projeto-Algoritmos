494f95b403de8f53c3be464d4f186dfb3fd1e2c7
Mario Azevedo <mariobct@msn.com>
2025-01-15 16:06:19 -0300

    tabuada com erro ... :(

--- 3.Tabuada.rb
+++ 3.Tabuada.rb
@@ -1,3 +1,12 @@
 # 3. Algoritmo com Loop para Tabuada
 # Objetivo: O programa pede um número e imprime a tabuada dele de 1 a 10.
 
+puts "---Tabuada---"
+puts "Digite um número: "
+num1 = gets.chomp.chomp.to_i
+
+loop do i = 0
+    puts "#{num1} x #{i} = #{num1 * i}"
+    i += 1
+end
+
